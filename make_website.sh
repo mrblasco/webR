578e1557dadb9a26318a002c17d2b2c33c44bb89
Andrea Blasco <ablasco@fas.harvard.edu>
2017-05-05 14:19:00 -0400

    Updated paper

--- make_website.sh
+++ make_website.sh
@@ -1,2 +1,3 @@
+#!/bin/bash
 Rscript -e "rmarkdown::render_site()"
  
---
+++ update_website.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# rn8mX4n549rFmQ
+#scp -r _site/* mrblasco@ablasco.org:~/public_html/
+scp -r _site/* ablasco@fas.harvard.edu:~/public_html/
